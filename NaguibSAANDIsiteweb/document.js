668520f8c200af84be1eb303862caae1ca071f49
NGB69200 <guibdesming@gmail.com>
2026-03-02 12:21:15 +0100

    Add files via upload

---
+++ NaguibSAANDIsiteweb/document.js
@@ -0,0 +1,6 @@
+document.title = "Le JavaScript";
+document.getElementById('bloc_page').style.width = '250px';
+var itemsListe1 = document.getElementsByClassName('items_liste1');
+var paragraphes = document.getElementsByTagName('p');
+document.querySelector('#bloc_page').style.backgroundColor = 'lightgrey';
+document.getElementById('p1').innerHTML = 'Nouveau texte du paragraphe 1' ;
